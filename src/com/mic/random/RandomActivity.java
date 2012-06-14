bf6444f4ad3242cf66294b5db0eacaea1917d1e9
MaidInChina <632907160@qq.com>
2012-06-15 00:38:35 +0800

    add SecondActivity, change button onclick method

--- src/com/mic/random/RandomActivity.java
+++ src/com/mic/random/RandomActivity.java
@@ -4,16 +4,22 @@ import android.app.Activity;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
-import android.view.View.OnClickListener;
+import android.view.ViewGroup;
 import android.view.Window;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.TableLayout;
+import android.widget.TableRow;
 
-public class RandomActivity extends Activity implements OnClickListener {
-	EditText textNumber;
-	Button buttonNext;
-	int eventNumber;
-	
+public class RandomActivity extends Activity {
+    EditText textNumber;
+    Button   buttonNext;
+    int      eventNumber;
+    
+    Button   buttonStart;
+    Button   buttonBack;
+    Button   buttonExit;
+    
     /** Called when the activity is first created. */
     @Override
     public void onCreate(Bundle savedInstanceState) {
@@ -21,19 +27,41 @@ public class RandomActivity extends Activity implements OnClickListener {
         this.requestWindowFeature(Window.FEATURE_NO_TITLE);
         setContentView(R.layout.number_chosen);
         
-        textNumber = (EditText)findViewById(R.id.number);
-        buttonNext = (Button)findViewById(R.id.next);
-        buttonNext.setOnClickListener(this);
+        textNumber = (EditText) findViewById(R.id.number);
+        // buttonNext = (Button) findViewById(R.id.next);
+        // buttonNext.setOnClickListener(this);
         
     }
-
-	@Override
-	public void onClick(View v) {
-		// TODO Auto-generated method stub
-		if(v == buttonNext){
-			eventNumber = Integer.parseInt(textNumber.getText().toString());
-	        setContentView(R.layout.main);
-		}
-
-	}
+    
+    public void nextClicked(View v) {
+        eventNumber = Integer.parseInt(textNumber.getText().toString());
+        // setContentView(R.layout.main);
+        Intent intent = new Intent(this, SecondActivity.class);
+        intent.putExtra("eventNumber", eventNumber);
+        startActivity(intent);
+        
+    }
+    
+    //
+    // @Override
+    // public void onClick(View v) {
+    // // TODO Auto-generated method stub
+    //
+    // int id = v.getId();
+    // switch (id) {
+    // case R.id.next: {
+    //
+    // }
+    // case R.id.start: {
+    //
+    // }
+    // case R.id.back:{
+    // setContentView(R.layout.number_chosen);
+    // }
+    // case R.id.quit:{
+    // System.exit(0);
+    // }
+    // }
+    //
+    // }
 }
